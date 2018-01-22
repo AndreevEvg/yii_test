a7de3e50b61650b0dae4ae35f0fcc2bd2831f6bd
Evgeniy Andreev <e.andreev@yandex.ru>
2018-01-22 17:47:27 +0300

    first commit

---
+++ controllers/PostController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\Post;
+
+class PostController extends AppController {
+    
+    public function actionIndex()
+    {   
+        //$posts = Post::find()->select('id, title, excerpt')->orderBy('id DESC')->all();
+        
+        //Пагинация постов
+        $query = Post::find()->select('id, title, excerpt')->orderBy('id DESC');
+        $pages = new \yii\data\Pagination(['totalCount' => $query->count(), 'pageSize' => 2]);
+        $posts = $query->offset($pages->offset)->limit($pages->limit)->all();
+        
+        return $this->render('index', [
+            'posts' => $posts,
+            'pages' => $pages,
+        ]);
+    }
+    
+    public function actionTest()
+    {
+        $test = "Hello Test!";
+        
+        return $this->render('test', [
+            'test' => $test,
+        ]);
+    }
+}
